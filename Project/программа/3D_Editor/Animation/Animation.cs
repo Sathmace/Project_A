51658fb309421808ad863f17a142e84f0b23b1c8
Ghitsh <sasha.mur01@mail.ru>
2023-03-13 15:10:21 +0300

    Update Animation.cs

--- Project/программа/3D_Editor/Animation/Animation.cs
+++ Project/программа/3D_Editor/Animation/Animation.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//comment
 namespace _3D_Editor.Animation
 {
     internal class Animation
     {
         public string _class_name { get; private set; }
-
+// 2 comment
         public Animation()
         {
+	
             _class_name = "Animation";
         }
     }
