2714e1486aff69f3af27bfabc206fb2e1b4503f1
Ghitsh <sasha.mur01@mail.ru>
2023-03-13 15:10:27 +0300

    Update Project_resources_controller.cs

--- Project/программа/3D_Editor/Project resources control/Project_resources_controller.cs
+++ Project/программа/3D_Editor/Project resources control/Project_resources_controller.cs
@@ -12,6 +12,7 @@ namespace _3D_Editor.Project_resources_control
 
         public Project_resources_controller()
         {
+	//my comm
             _class_name = "Project_resources_controller";
         }
     }
